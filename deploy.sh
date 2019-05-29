c2d2fc65ee52e83067460218e854ded987d4b41b
Pedro Martin <pataruco@gmail.com>
2019-05-29 11:21:40 +0100

    Create api getway

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 cd src
-zip ../example.zip main.js
+zip ../dates.zip dates.js
+zip ../date.zip dates.js 
 cd ..
-aws s3 cp example.zip s3://peter-of-the-day-staging/v1.0.0/example.zip
+aws s3 cp example.zip s3://peter-of-the-day-staging/v1.0.0/dates.zip
+aws s3 cp example.zip s3://peter-of-the-day-staging/v1.0.0/date.zip
--- infrastructure/staging/lambda.tf
+++ infrastructure/staging/lambda.tf
@@ -1,14 +1,31 @@
-resource "aws_lambda_function" "example" {
-  function_name = "ServerlessExample"
+resource "aws_lambda_function" "dates" {
+  function_name = "dates"
 
   # The bucket name as created earlier with "aws s3api create-bucket"
   s3_bucket = "peter-of-the-day-staging"
-  s3_key    = "v1.0.0/example.zip"
+  s3_key    = "v1.0.0/dates.zip"
 
   # "main" is the filename within the zip file (main.js) and "handler"
   # is the name of the property under which the handler function was
   # exported in that file.
-  handler = "main.handler"
+  handler = "dates.handler"
+
+  runtime = "nodejs10.x"
+
+  role = "${aws_iam_role.lambda_exec.arn}"
+}
+
+resource "aws_lambda_function" "date" {
+  function_name = "date"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "peter-of-the-day-staging"
+  s3_key    = "v1.0.0/date.zip"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "date.handler"
 
   runtime = "nodejs10.x"
 
