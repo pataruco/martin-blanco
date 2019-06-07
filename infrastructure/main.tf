609cf7d9236c6e16081541b421fae2360b8a441c
Pedro Martin <pataruco@gmail.com>
2019-06-07 12:58:40 +0100

    Refactor terraform folders

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,3 +15,53 @@ variable "POD_BUCKET_NAME" {}
 variable "node_version" {
   default = "nodejs10.x"
 }
+
+variable "stage_name" {
+  default = "api"
+}
+
+resource "aws_api_gateway_rest_api" "martin_blanco" {
+  name        = "Martin Blanco API"
+  description = "Martin Blanco API"
+}
+
+data "archive_file" "lambdas_zip" {
+  type        = "zip"
+  source_dir  = "../src/lamdas"
+  output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "date" {
+  filename         = "${data.archive_file.lambdas_zip.output_path}"
+  function_name    = "date"
+  role             = "${aws_iam_role.lambda_exec.arn}"
+  handler          = "date.handler"
+  runtime          = "${var.node_version}"
+  source_code_hash = "${base64sha256(file("${data.archive_file.lambdas_zip.output_path}"))}"
+  publish          = true
+  timeout          = 10
+  layers           = ["${aws_lambda_layer_version.node_dependencies.arn}"]
+
+  environment {
+    variables = {
+      POD_BUCKET_NAME = "${var.POD_BUCKET_NAME}"
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name               = "lamda_execution_role"
+  assume_role_policy = "${file("policies/lambda-role.json")}"
+}
+
+module "dates" {
+  source                 = "./routes/dates"
+  rest_api_id            = "${aws_api_gateway_rest_api.martin_blanco.id}"
+  root_id                = "${aws_api_gateway_rest_api.martin_blanco.root_resource_id}"
+  stage_name             = "${var.stage_name}"
+  lamda_role             = "${aws_iam_role.lambda_exec.arn}"
+  node_version           = "${var.node_version}"
+  POD_BUCKET_NAME        = "${var.POD_BUCKET_NAME}"
+  zip_output_path        = "${data.archive_file.lambdas_zip.output_path}"
+  rest_api_execution_arn = "${aws_api_gateway_rest_api.martin_blanco.execution_arn}"
+}
--- infrastructure/routes/dates/apigateway.tf
+++ infrastructure/routes/dates/apigateway.tf
@@ -1,29 +1,28 @@
-resource "aws_api_gateway_rest_api" "martin_blanco" {
-  name        = "Martin Blanco API"
-  description = "Martin Blanco API"
-}
+variable rest_api_id {}
+variable root_id {}
+variable stage_name {}
+variable rest_api_execution_arn {}
 
 resource "aws_api_gateway_resource" "dates_resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.martin_blanco.id}"
-  parent_id   = "${aws_api_gateway_rest_api.martin_blanco.root_resource_id}"
+  rest_api_id = "${var.rest_api_id}"
+  parent_id   = "${var.root_id}"
   path_part   = "dates"
 }
 
 resource "aws_api_gateway_method" "dates_method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.martin_blanco.id}"
+  rest_api_id   = "${var.rest_api_id}"
   resource_id   = "${aws_api_gateway_resource.dates_resource.id}"
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "dates_lambda" {
-  rest_api_id             = "${aws_api_gateway_rest_api.martin_blanco.id}"
+  rest_api_id             = "${var.rest_api_id}"
   resource_id             = "${aws_api_gateway_resource.dates_resource.id}"
   http_method             = "${aws_api_gateway_method.dates_method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-
-  uri = "${aws_lambda_function.dates.invoke_arn}"
+  uri                     = "${aws_lambda_function.dates.invoke_arn}"
 }
 
 resource "aws_api_gateway_deployment" "dates_deployment" {
@@ -32,8 +31,8 @@ resource "aws_api_gateway_deployment" "dates_deployment" {
     "aws_api_gateway_integration.dates_lambda",
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.martin_blanco.id}"
-  stage_name  = "staging"
+  rest_api_id = "${var.rest_api_id}"
+  stage_name  = "${var.stage_name}"
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -42,7 +41,5 @@ resource "aws_lambda_permission" "apigw" {
   function_name = "${aws_lambda_function.dates.arn}"
   principal     = "apigateway.amazonaws.com"
 
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${replace(aws_api_gateway_deployment.dates_deployment.execution_arn, aws_api_gateway_deployment.dates_deployment.stage_name, "")}*/*"
+  source_arn = "${var.rest_api_execution_arn}/*/*/*"
 }
