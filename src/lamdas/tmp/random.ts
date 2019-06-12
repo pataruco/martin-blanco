72b5b089ac44fb49f4e582c97fdbb1d0a8acd244
Pedro Martin <pataruco@gmail.com>
2019-06-12 09:31:23 +0100

    Create random tsc

---
+++ src/lamdas/tmp/random.ts
@@ -0,0 +1,37 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { getManifest, getPicture } from './lib';
+
+const getRandomNumber = (maxNumber: number): number =>
+  Math.floor(Math.random() * maxNumber) + 1;
+
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+  const manifest = await getManifest();
+  if (manifest && manifest.dates) {
+    const maxDatesNumber = manifest.dates.length;
+    const randomDateNumber = getRandomNumber(maxDatesNumber);
+    const randomDate = manifest.dates[randomDateNumber - 1];
+    const maxFileNumber = randomDate.files.length;
+    const randomFileNumber = getRandomNumber(maxFileNumber);
+    const file = randomDate.files[randomFileNumber - 1].url;
+
+    if (file) {
+      return {
+        isBase64Encoded: true,
+        statusCode: 200,
+        headers: {
+          'Content-type': 'image/jpeg',
+        },
+        body: await getPicture(file),
+      };
+    }
+  }
+
+  return {
+    isBase64Encoded: true,
+    statusCode: 400,
+    headers: {},
+    body: JSON.stringify({
+      message: 'file not found',
+    }),
+  };
+};
