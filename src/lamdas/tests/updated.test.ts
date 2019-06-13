d441c670fea2657c688d4ee2539a11a528c2ffaf
Pedro Martin <pataruco@gmail.com>
2019-06-13 20:20:37 +0100

    Test updated

---
+++ src/lamdas/tests/updated.test.ts
@@ -0,0 +1,18 @@
+import { handler as updated } from '../updated';
+
+describe(updated, () => {
+  it('return an updated date', async () => {
+    const response = await updated();
+
+    const updateDate = '2019-01-20T15:11:02.616+00:00';
+
+    expect(response).toEqual({
+      statusCode: 200,
+      headers: {},
+      isBase64Encoded: false,
+      body: JSON.stringify({
+        updated: updateDate,
+      }),
+    });
+  });
+});
