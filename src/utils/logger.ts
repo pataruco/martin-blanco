f2f9f9228c317ec4d8ccf96acd5c5a5861373716
Pedro Martin <pedro.martin@red-badger.com>
2020-02-15 22:07:34 +0000

    Clean server

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,7 @@
 import { createLogger, format, transports } from 'winston';
 const { label, combine, timestamp, prettyPrint } = format;
+// TODO: Add Google StackDriver integration https://github.com/winstonjs/winston/blob/master/docs/transports.md#google-stackdriver-transport
+
 const logger = createLogger({
   format: combine(timestamp(), prettyPrint(), label()),
   transports: [new transports.Console()],
