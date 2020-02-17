4c297e2ada1d1fa2991d8ffced3e3d7b54f8288d
Pedro Martin <pedro.martin@red-badger.com>
2020-02-17 23:04:47 +0000

    Parametise project id on deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,5 @@ set -euo pipefail
 
 CLOUD_FUNCTION=martin-blanco-api
 SOURCE=./api
-CLOUD_PROJECT=martin-blanco-api-dev
 
 gcloud functions deploy $CLOUD_FUNCTION --region=europe-west2 --source=$SOURCE --trigger-http --project $CLOUD_PROJECT --entry-point app --verbosity=info --runtime nodejs10
