517950d4adef232cbc2f2d249127db238df741f6
Pedro Martin <pedro.martin@red-badger.com>
2020-02-21 20:30:44 +0000

    Draft getAllFiles

--- src/importer/index.ts
+++ src/importer/index.ts
@@ -1,1 +1,13 @@
-console.log('hola');
+import path from 'path';
+import { promises as fs } from 'fs';
+
+const inputPath = path.resolve(__dirname, 'input');
+const outputPath = path.resolve(__dirname, 'output');
+
+const getAllFiles = async () => await fs.readdir(inputPath);
+
+const readfiles = async () => {
+  await (await getAllFiles()).forEach(file => console.log({ file }));
+};
+
+readfiles();
