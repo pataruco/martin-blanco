15fb7974e835716777e9bdce0e6782584f677910
Pedro Martin <pedro.martin@red-badger.com>
2020-02-23 09:57:12 +0000

    Add env variables to CI

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -5,4 +5,4 @@ CLOUD_FUNCTION=martin-blanco-api
 SOURCE=./api
 CLOUD_PROJECT=martin-blanco-api-prod
 
-gcloud functions deploy $CLOUD_FUNCTION --region=europe-west2 --source=$SOURCE --trigger-http --project $CLOUD_PROJECT --entry-point app --verbosity=info --runtime nodejs10
+gcloud functions deploy $CLOUD_FUNCTION --region=europe-west2 --source=$SOURCE --trigger-http --project $CLOUD_PROJECT --entry-point app --verbosity=info --runtime nodejs10 --set-env-vars PROJECT_ID="${CLOUD_PROJECT}",BUCKET_NAME="${BUCKET_NAME}"
