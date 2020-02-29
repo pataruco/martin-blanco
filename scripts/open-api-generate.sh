46144c8246a71f604e8b0a9809624bf2deb65b56
Pedro Martin <pedro.martin@red-badger.com>
2020-02-29 17:19:54 +0000

    remove open api documentation script

--- scripts/open-api-generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Create Open API HTML
-yarn redoc-cli bundle ./src/open-api/open-api-schema.json --options.theme.colors.primary.main=black
-
-# Move file to open-api folder
-mv ./redoc-static.html ./src/open-api/index.html
