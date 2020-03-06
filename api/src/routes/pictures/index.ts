768009a83a20ccbd8b1fb580cfd0fdcce43df905
Pedro Martin <pedro.martin@red-badger.com>
2020-03-06 14:50:48 +0000

    Add stackdriver logger

--- api/src/routes/pictures/index.ts
+++ api/src/routes/pictures/index.ts
@@ -1,12 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import express, { Request, Response } from 'express';
 import Joi from '@hapi/joi';
-import {
-  getFilesBy,
-  getFileById,
-  getRandomFile,
-  getStoragePaths,
-} from '../../models/picture';
+import { getFilesBy, getFileById, getRandomFile } from '../../models/picture';
 
 const router = express.Router();
 
@@ -199,30 +194,10 @@ const getRandomPicture = async (
   }
 };
 
-// FIXME: Check why is not uploading on Google Cloud
-const uploadPictures = async (
-  req: Request,
-  res: Response,
-): Promise<Response> => {
-  try {
-    const { files } = await req;
-    // @ts-ignore
-    const storagePaths = await getStoragePaths(files);
-    return res.status(201).json({
-      filesUploaded: storagePaths,
-    });
-  } catch (error) {
-    return res.status(409).json({
-      message: `Error uploadPictures, Error: ${error}`,
-    });
-  }
-};
-
 router.get('/pictures/date/:year', getPicturesByYear);
 router.get('/pictures/date/:year/:month', getPicturesByMonth);
 router.get('/pictures/date/:year/:month/:day', getPicturesByDay);
 router.get('/pictures/date/:year/:month/:day/:id', getPictureById);
 router.get('/pictures/random', getRandomPicture);
-router.post('/pictures', uploadPictures);
 
 export default router;
