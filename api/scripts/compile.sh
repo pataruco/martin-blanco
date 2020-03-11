311ef7fd692d92c084dc9b0e8fcde77a4da5cf05
Pedro Martin <pedro.martin@red-badger.com>
2020-03-11 22:13:43 +0000

    Remove npmrc

--- api/scripts/compile.sh
+++ api/scripts/compile.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Copy .npmrc
-echo registry=https://npm.pkg.github.com/@pataruco >>./dist/.npmrc
-echo //npm.pkg.github.com/:_authToken=${GITHUB_REGISTRY_AUTH_TOKEN} >>./dist/.npmrc
-
 # Copy package json
 cp ./package.json ./dist
 
