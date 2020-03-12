0f05f097d6f350c9856913755963480209d62013
Pedro Martin <pedro.martin@red-badger.com>
2020-03-12 18:48:47 +0000

    Change source on deploy prod script

--- api/scripts/deploy-prod.sh
+++ api/scripts/deploy-prod.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 CLOUD_FUNCTION=martin-blanco-api
-SOURCE=./api
+SOURCE=.
 CLOUD_PROJECT=martin-blanco-api-prod
 
 gcloud functions deploy $CLOUD_FUNCTION --region=europe-west2 --source=$SOURCE --trigger-http --project $CLOUD_PROJECT --entry-point app --verbosity=info --runtime nodejs10 --set-env-vars BUCKET_NAME="${BUCKET_NAME}"
