d576c55c96c6b824bb11833ef002cf77e0124b91
Pedro Martin <pedro.martin@red-badger.com>
2020-03-12 14:32:14 +0000

    Bring shared library

--- upload-cli/src/uploader.ts
+++ upload-cli/src/uploader.ts
@@ -1,5 +1,5 @@
 import { ExifParserFactory } from 'ts-exif-parser';
-import { getFilesBy } from '@pataruco/shared';
+// import { getFilesBy } from '@pataruco/shared';
 import { promises as fs } from 'fs';
 import { Storage } from '@google-cloud/storage';
 import dotenv from 'dotenv';
@@ -30,6 +30,39 @@ interface UploadFilePath extends OriginalTime {
 }
 
 const storage = new Storage();
+
+export interface Time {
+  year: string;
+  month?: string;
+  day?: string;
+}
+
+export const getDirectory = (query: Time) => {
+  const { year, month, day } = query;
+  return day
+    ? `pictures/${year}/${month}/${day}`
+    : month
+    ? `pictures/${year}/${month}`
+    : `pictures/${year}`;
+};
+
+export const getFilesBy = async (query: Time) => {
+  const directory = getDirectory(query);
+
+  const [allFiles] = await storage.bucket(`${BUCKET_NAME}`).getFiles({
+    autoPaginate: false,
+    directory,
+  });
+
+  return await Promise.all(
+    allFiles
+      .filter(file => file.name.includes('.'))
+      .map(
+        async file =>
+          `https://storage.googleapis.com/${BUCKET_NAME}/${file.name}`,
+      ),
+  );
+};
 
 const getFilesByPath = async (path: string): Promise<string[]> => {
   try {
