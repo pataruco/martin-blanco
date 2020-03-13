883eac1d7be17ceb74fc704a18e8437426b87e77
Pedro Martin <pedro.martin@red-badger.com>
2020-03-13 15:54:41 +0000

    Set coalescense for PORT

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,8 +7,8 @@ import fileUploadMiddleware from './middleware/file-uploads';
 import logger from './utils/logger';
 import router from './routes';
 
-export const PORT = process.env.PORT || '8080';
-export const HOST = process.env.HOST || '0.0.0.0';
+export const PORT = process.env.PORT ?? '8080';
+export const HOST = process.env.HOST ?? '0.0.0.0';
 
 export const app = express();
 
