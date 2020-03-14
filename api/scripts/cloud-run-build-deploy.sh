578aa0652e3446c8b64f575f74adb89c41c6bd8f
Pedro Martin <pedro.martin@red-badger.com>
2020-03-14 18:45:29 +0000

    Add diggest

--- api/scripts/cloud-run-build-deploy.sh
+++ api/scripts/cloud-run-build-deploy.sh
@@ -7,5 +7,8 @@ docker build . --tag gcr.io/${PROJECT}/${SERVICE_NAME}
 # Docker Push
 docker push gcr.io/${PROJECT}/${SERVICE_NAME}
 
+#Digest
+export DIGEST=$(docker image ls --digests gcr.io/${PROJECT}/${SERVICE_NAME} --format "{{.Digest}}")
+
 # GCloud run deploy
 gcloud run deploy ${SERVICE_NAME} --set-env-vars BUCKET_NAME=${BUCKET_NAME} --image gcr.io/${PROJECT}/${SERVICE_NAME}
