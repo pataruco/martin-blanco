b6df50c1c3704668b06a75c28a8e11aebc58f56b
Pedro Martin <pedro.martin@red-badger.com>
2020-03-14 21:19:54 +0000

    Draft Cloud Run TF module

---
+++ infrastructure/modules/cloud-run/main.tf
@@ -0,0 +1,47 @@
+resource "google_cloud_run_service" "martin_blanco_api" {
+  name     = var.service_name
+  location = "europe-west1"
+  project  = var.project
+
+  # name    = "martin-blanco-api"
+  # project = "martin-blanco-api-dev"
+
+  metadata {
+    namespace = var.project
+  }
+
+  template {
+    spec {
+      containers {
+        image = var.image
+        # image = "${var.image}@${var.digest}"
+        env {
+          name  = "BUCKET_NAME"
+          value = var.bucket_name
+        }
+      }
+    }
+  }
+
+  traffic {
+    latest_revision = true
+    percent         = 100
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.martin_blanco_api.location
+  project  = google_cloud_run_service.martin_blanco_api.project
+  service  = google_cloud_run_service.martin_blanco_api.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
