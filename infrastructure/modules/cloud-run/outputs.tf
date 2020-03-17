2f61c6483ea65fcdb1e6ad06602adaf9c83f077d
Pedro Martin <pedro.martin@red-badger.com>
2020-03-17 20:35:26 +0000

    Add a custom mapping for cloud run

--- infrastructure/modules/cloud-run/outputs.tf
+++ infrastructure/modules/cloud-run/outputs.tf
@@ -1,3 +1,7 @@
 output "cloud_run_status" {
   value = google_cloud_run_service.martin_blanco_api.status
 }
+
+output "cloud_run_custom_domain_status" {
+  value = google_cloud_run_domain_mapping.custom_domain.status
+}
