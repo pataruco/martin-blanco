176da61ad3a4e51992dcf1adcc0bc777d690baf0
Pedro Martin <pedro.martin@red-badger.com>
2020-03-20 16:54:39 +0000

    remove file upload library

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,6 @@
 import express from 'express';
 import errorHandlerMiddleware from './middleware/error-handler';
 import httpLoggerMiddleware from './middleware/http-logger';
-import fileUploadMiddleware from './middleware/file-uploads';
 
 import logger from './utils/logger';
 import router from './routes';
@@ -13,7 +12,6 @@ export const HOST = process.env.HOST ?? '0.0.0.0';
 export const app = express();
 
 app.use(httpLoggerMiddleware);
-app.use(fileUploadMiddleware.array('files'));
 app.use(errorHandlerMiddleware);
 
 // Routes
