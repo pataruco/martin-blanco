43d10e383b2252cd870cf9dff256f6b291f52765
Pedro Martin <pedro.martin@red-badger.com>
2020-03-21 13:13:55 +0000

    Test /

--- api/src/routes/index.test.ts
+++ api/src/routes/index.test.ts
@@ -1,5 +1,12 @@
-describe('/pictures', () => {
-  it('returns true', () => {
-    expect(true).toBe(true);
+import app from '../index';
+import request from 'supertest';
+
+jest.mock('../utils/logger');
+
+describe('/', () => {
+  it('return 200 with an open API Swagger HTML reference', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text.includes('swagger')).toBe(true);
   });
 });
