8cbcd4f43030a1d73d73eadf7d0643fb8a0167d3
Pedro Martin <pedro.martin@red-badger.com>
2020-03-21 18:15:22 +0000

    Test 'GET /pictures/date/:year' 200 OK

--- api/src/routes/pictures/index.test.ts
+++ api/src/routes/pictures/index.test.ts
@@ -4,11 +4,25 @@ import * as picture from '../../models/picture';
 
 jest.mock('../../utils/logger');
 
+const files = [
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/01/1.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/04/1.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/05/1.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/06/1.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/08/1.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/10/1.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/10/2.jpg',
+  'https://storage.googleapis.com/martin-blanco/pictures/2018/01/10/3.jpg',
+];
+
 const getFilesByReturnsEmpty = () =>
   jest.spyOn(picture, 'getFilesBy').mockResolvedValueOnce([]);
 
 const getFilesByThrowError = () =>
   jest.spyOn(picture, 'getFilesBy').mockRejectedValueOnce(new Error('💥'));
+
+const getFilesByReturnsFiles = () =>
+  jest.spyOn(picture, 'getFilesBy').mockResolvedValueOnce(files);
 
 describe('/pictures', () => {
   describe('GET /pictures/date/:year', () => {
@@ -17,7 +31,7 @@ describe('/pictures', () => {
       ['2016', '"year" must be larger than or equal to 2017'],
     ];
     it.each(badRequests)(
-      'returns 422 with message when requests fail validation (/pictures/date/%s)',
+      'returns 422 with message when requests fail validation (/pictures/date/%s) ❌',
       async (reqParam: string, message: string) => {
         const response = await request(app).get(`/pictures/date/${reqParam}`);
         expect(response.status).toBe(422);
@@ -25,7 +39,7 @@ describe('/pictures', () => {
       },
     );
 
-    it('returns 404 when files are not found', async () => {
+    it('returns 404 when files are not found ❌', async () => {
       getFilesByReturnsEmpty();
       const year = '2020';
       const response = await request(app).get(`/pictures/date/${year}`);
@@ -33,13 +47,24 @@ describe('/pictures', () => {
       expect(response.body).toEqual({ message: 'Files not found', year });
     });
 
-    it('returns 500 when getFilesBy throw an error', async () => {
+    it('returns 500 when getFilesBy throw an error ❌', async () => {
       getFilesByThrowError();
       const year = '2020';
       const response = await request(app).get(`/pictures/date/${year}`);
       expect(response.status).toBe(500);
       expect(response.body).toEqual({
         message: 'Error getFilesByYear, Error: Error: 💥',
+      });
+    });
+
+    it('returns 200 with a list of files ✅', async () => {
+      getFilesByReturnsFiles();
+      const year = '2020';
+      const response = await request(app).get(`/pictures/date/${year}`);
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({
+        year,
+        files,
       });
     });
   });
