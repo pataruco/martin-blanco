e367f5631d1ad0f70e6256464b41ffb7e464eb8c
Pedro Martin <pedro.martin@red-badger.com>
2020-03-21 16:43:32 +0000

    test files not found for /pictures/date/:year

--- api/src/routes/pictures/index.test.ts
+++ api/src/routes/pictures/index.test.ts
@@ -1,17 +1,18 @@
 import app from '../../index';
 import request from 'supertest';
+import * as picture from '../../models/picture';
 
 jest.mock('../../utils/logger');
 
 describe('/pictures', () => {
-  describe('/pictures/date/:year', () => {
-    describe('retuns 422 with a validation message when requests fail validation', () => {
+  describe('GET /pictures/date/:year', () => {
+    describe('returns 422 with message when requests fail validation', () => {
       const badRequests = [
         ['test', '"year" must be a number'],
         ['2016', '"year" must be larger than or equal to 2017'],
       ];
       it.each(badRequests)(
-        'request.app.get(/pictures/date/%s)',
+        '/pictures/date/%s',
         async (reqParam: string, message: string) => {
           const response = await request(app).get(`/pictures/date/${reqParam}`);
           expect(response.status).toBe(422);
@@ -19,5 +20,21 @@ describe('/pictures', () => {
         },
       );
     });
+
+    it('returns 404 when files are not found', async () => {
+      jest.spyOn(picture, 'getFilesBy').mockResolvedValueOnce([]);
+      const year = '2020';
+      const response = await request(app).get(`/pictures/date/${year}`);
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({ message: 'Files not found', year });
+    });
+
+    // it('returns 404 when files are not found', async () => {
+    //   jest.spyOn(picture, 'getFilesBy').mockResolvedValueOnce([]);
+    //   const year = '2020';
+    //   const response = await request(app).get(`/pictures/date/${year}`);
+    //   expect(response.status).toBe(404);
+    //   expect(response.body).toEqual({ message: 'Files not found', year });
+    // });
   });
 });
