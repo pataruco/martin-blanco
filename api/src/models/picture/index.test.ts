adfc2be41b14d064aa2cc6e278b375208ac9521c
Pedro Martin <pedro.martin@red-badger.com>
2020-03-21 13:02:26 +0000

    amend picture model test

--- api/src/models/picture/index.test.ts
+++ api/src/models/picture/index.test.ts
@@ -1,7 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { getDirectory, Time, getOriginalTime, getNumberString } from './';
-import { promises as fs } from 'fs';
-import path from 'path';
+import { getDirectory, Time, getNumberString } from './';
 
 describe(getDirectory, () => {
   const year = '2017';
@@ -16,19 +14,6 @@ describe(getDirectory, () => {
 
   it.each(directories)('getDirectory(%o)', (query: Time, directory: string) => {
     expect(getDirectory(query)).toBe(directory);
-  });
-});
-
-describe(getOriginalTime, () => {
-  it('return an originalTime object', async () => {
-    const filePath = path.resolve(__dirname, './test-helpers', '1.jpeg');
-    const file = await fs.readFile(filePath);
-
-    expect(await getOriginalTime(file)).toEqual({
-      year: 1970,
-      month: 0,
-      day: 1,
-    });
   });
 });
 
