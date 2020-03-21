d5479c3bcd0a90804efede80f66fad69ef2e0972
Pedro Martin <pedro.martin@red-badger.com>
2020-03-21 12:21:31 +0000

    Enable supertest to test /health

--- api/src/routes/health/index.test.ts
+++ api/src/routes/health/index.test.ts
@@ -1,39 +1,13 @@
-import nock from 'nock';
-import app, { PORT, HOST } from '../../index';
-import fetch from 'node-fetch';
+import app from '../../index';
+import request from 'supertest';
+
+jest.mock('../../utils/logger');
 
 describe('/health', () => {
-  let application;
-  let server;
-  let baseUri: string;
-
-  beforeEach(done => {
-    application = app;
-    baseUri = `http://${HOST}:${PORT}`;
-    server = application.listen(0, done);
-    nock.disableNetConnect();
-    nock.cleanAll();
-  });
-
-  afterEach(done => {
-    nock.enableNetConnect();
-    server.close(done);
-  });
-
-  afterAll(done => {
-    server.close(done);
-  });
-
   it('returns 200 OK', async () => {
-    nock(baseUri)
-      .get('/health')
-      .reply(200, 'OK');
+    const response = await request(app).get('/health');
 
-    const response = await fetch(`${baseUri}/health`);
-
-    const text = await response.text();
-
-    expect(text).toBe('OK');
     expect(response.status).toBe(200);
+    expect(response.text).toBe('OK');
   });
 });
