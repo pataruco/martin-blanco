486481026c1f7067be39ca258820ca3c55277b24
Pedro Martin <pedro.martin@red-badger.com>
2020-03-22 18:01:38 +0000

    draft transcoder

--- upload-cli/src/movies/uploader.ts
+++ upload-cli/src/movies/uploader.ts
@@ -1,7 +1,10 @@
 import ffmpeg from 'fluent-ffmpeg';
 import { getFilesByPath, getFilePath, OriginalTime } from '../lib/uploader';
+// var stream = require('stream').Writable;
+import { Writable } from 'stream';
 // import path from 'path';
 
+import fs from 'fs';
 const source = '/Users/pataruco/Desktop/movies';
 
 // const ffmpeg = Ffmpeg();
@@ -33,32 +36,43 @@ const getOriginalTime = async (filePath: string): Promise<OriginalTime> => {
   });
 };
 
+const getTranscodedBuffer = async (filePath: string): Promise<Writable> =>
+  new Promise((resolve, reject) => {
+    const stream = new Writable();
+    ffmpeg()
+      .input(filePath)
+      .size('50%')
+      .format('mp4')
+      .writeToStream(stream, { end: true })
+      .on('start', () => console.log(`🟢 Start Transcoding ${filePath}`))
+      .on('progress', progress =>
+        console.log(`Processing: ${progress.percent} done`),
+      )
+      .on('end', () => {
+        console.log(' 🏁Transcoding succeeded !');
+        resolve(stream);
+      })
+      .on('error', () => {
+        return reject(new Error(`Failed to transcode file ${filePath}`));
+      });
+  });
+
 const start = async () => {
-  const filesPath = await getFilesByPath(source);
-  let counter = 0;
+  try {
+    const filesPath = await getFilesByPath(source);
 
-  for (const file of filesPath) {
-    const filePath = getFilePath({ file, source });
-    // create path format
-    const { year, month, day } = await getOriginalTime(filePath);
+    for (const file of filesPath) {
+      const filePath = getFilePath({ file, source });
+      // create path format
+      const { year, month, day } = await getOriginalTime(filePath);
 
-    console.log({ filePath, year, month, day });
+      const rotateAndResizeBuffer = await getTranscodedBuffer(filePath);
 
-    // const ffmpeg = Ffmpeg();
-    // ffmpeg()
-    //   .input(filePath)
-    // .size('50%')
-    // .format('mp4')
-    // .on('progress', progress => {
-    //   console.log('Processing: ' + progress.percent + '% done');
-    // })
-    // .ffprobe((err, data) => {
-    // @ts-ignore
-    // console.dir(data.format.tags['com.apple.quicktime.creationdate']);
-    // });
-    // .save(`./${counter}.mp4`);
-
-    // console.log('complete');
+      console.log({ rotateAndResizeBuffer });
+    }
+  } catch (e) {
+    console.error(e);
+    throw Error(`Error processing upload. ${e}`);
   }
 };
 
