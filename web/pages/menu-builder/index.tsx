c3703eb7074206634ac025c3d29acbac81b20757
Pedro Martin <pedro.martin@red-badger.com>
2020-06-07 20:06:26 +0100

    Add table

--- web/pages/menu-builder/index.tsx
+++ web/pages/menu-builder/index.tsx
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const menuBuilderPage = () => <h1>Menu builder</h1>;
+const menuBuilderPage: React.FC = () => (
+  <Fragment>
+    <h1>Week menu planner</h1>
+    <table>
+      <thead>
+        <tr>
+          <td>Meal</td>
+          <td>Monday</td>
+          <td>Tuesday</td>
+          <td>Wednesday</td>
+          <td>Thursday</td>
+          <td>Friday</td>
+          <td>Saturday</td>
+          <td>Sunday</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Breakfast</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Lunch</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Dinner</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </Fragment>
+)
 
 export default menuBuilderPage;
